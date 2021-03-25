c8c7268fecb660ae7689dd5dd5b1592840f2c043
pade88 <ovidiu.padeanu@gmail.com>
2021-03-25 23:29:09 +0200

    Initial commit of new app

---
+++ app/src/main/java/com/example/rdproiectamdm/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.rdproiectamdm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        FetchData();
+    }
+
+    private void FetchData()
+    {
+        // ToDo
+    }
+}
+
