24afe5243bf8105df531e20fd4ae0ae7db932999
pade88 <ovidiu.padeanu@gmail.com>
2021-04-26 01:51:50 +0300

    Branch 1 implementation

--- app/src/main/java/com/example/rdproiectamdm/CSVData.java
+++ app/src/main/java/com/example/rdproiectamdm/CSVData.java
@@ -22,17 +22,30 @@ public class CSVData
     public CSVData(String[] params)
     {
         // Despachetare
-        this.nume = params[0];
-        this.regiune = params[1];
-        this.cazuriTotale = Integer.parseInt(params[2]);
-        this.cazuriPerMilion = Float.parseFloat(params[3]);
-        this.cazuriUltimele7zile = Integer.parseInt(params[4]);
-        this.cazuriUltimele7zilePerMilion = Float.parseFloat(params[5]);
-        this.cazuriUltimele24ore = Integer.parseInt(params[6]);;
-        this.mortiTotal = Integer.parseInt(params[7]);;
-        this.mortiPerMilion = Float.parseFloat(params[8]);
-        this.mortiUltimele7zile = Integer.parseInt(params[9]);
-        this.mortiUltimele7zilePerMilion = Float.parseFloat(params[10]);;
-        this.mortiUltimele24ore = Integer.parseInt(params[11]);
+        this.nume = (params[0].equals("")? "Unknown":params[0]);
+        this.regiune = (params[1].equals("")? "Unknown":params[1]);
+        this.cazuriTotale = (params[2].equals("")? 0 : Integer.parseInt(params[2]));
+        this.cazuriPerMilion = (params[3].equals("")? 0.0f: Float.parseFloat(params[3]));
+        this.cazuriUltimele7zile = (params[4].equals("")? 0 : Integer.parseInt(params[4]));
+        this.cazuriUltimele7zilePerMilion = (params[5].equals("")? 0.0f: Float.parseFloat(params[5]));
+        this.cazuriUltimele24ore = (params[6].equals("")? 0 : Integer.parseInt(params[6]));
+        this.mortiTotal = (params[7].equals("")? 0 : Integer.parseInt(params[7]));
+        this.mortiPerMilion = (params[8].equals("")? 0.0f: Float.parseFloat(params[8]));
+        this.mortiUltimele7zile = (params[9].equals("")? 0 : Integer.parseInt(params[9]));
+        this.mortiUltimele7zilePerMilion = (params[10].equals("")? 0.0f: Float.parseFloat(params[10]));
+        this.mortiUltimele24ore = (params[11].equals("")? 0 : Integer.parseInt(params[11]));
     }
+
+    public String getNumeTara()
+    {
+        return this.nume;
+    }
+
+    public int getCazuriTotale() { return this.cazuriTotale; }
+
+    public String getRegiune() {return this.regiune; }
+
+    public int getDecese() {return this.mortiTotal; }
+
+    public int getCazuriUltimele24ore() {return this.cazuriUltimele24ore; }
 }
--- app/src/main/java/com/example/rdproiectamdm/FirstFragment.java
+++ app/src/main/java/com/example/rdproiectamdm/FirstFragment.java
@@ -1,31 +1,31 @@
 package com.example.rdproiectamdm;
 
-import android.os.Build;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
-import com.opencsv.CSVReader;
-
-import org.w3c.dom.Text;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Vector;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -76,9 +76,211 @@ public class FirstFragment extends Fragment {
         }
     }
 
-    public List<CSVData> UploadData() throws IOException
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
     {
-        List<CSVData> ListaTari = null;
+        View rootView = inflater.inflate(R.layout.fragment_first, container, false);
+
+        Vector<CSVData> objectList = new Vector<CSVData>();
+        try
+        {
+            objectList = UploadData();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+
+        // Pie Charts
+
+        initPieCharts(rootView);
+
+        chart1.setData(generatePieDataContinent(objectList, "Europe", 10));
+        chart2.setData(generatePieDataContinent(objectList, "Western Pacific", 5));
+        chart3.setData(generatePieDataContinent(objectList, "Americas", 5));
+        chart4.setData(generatePieDataContinent(objectList, "Africa", 5));
+        chart5.setData(generatePieTops(objectList, "TOP3TOTAL"));
+        chart6.setData(generatePieTops(objectList, "TOP3DEATH"));
+        chart7.setData(generatePieTops(objectList, "TOP324H"));
+
+        TextView CNT = rootView.findViewById(R.id.textView);
+        final Random myRandom = new Random();
+        AtomicInteger sum = new AtomicInteger(getTotalCasesSum(objectList));
+
+        Thread counterThread=new Thread(()->{
+            try{
+                while(true)
+                {
+                    sum.addAndGet(myRandom.nextInt(500));
+                    CNT.setText("Numar total de cazuri pe planeta: "  + sum.toString());
+                    Thread.sleep(1000);
+                }
+            }
+            catch (Exception e){
+
+            }
+        });
+        counterThread.start();
+        return rootView;
+    }
+
+    public int getTotalCasesSum(Vector<CSVData> function_input)
+    {
+        for(CSVData looper : function_input)
+        {
+            if(looper.getNumeTara().equals("Global"))
+            {
+                return looper.getCazuriTotale();
+            }
+        }
+        return 0;
+    }
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private Typeface tf;
+    private PieChart chart1;
+    private PieChart chart2;
+    private PieChart chart3;
+    private PieChart chart4;
+    private PieChart chart5;
+    private PieChart chart6;
+    private PieChart chart7;
+
+    public void initPieCharts(View viewID)
+    {
+        Vector<PieChart> charts = new Vector<PieChart>();
+        chart1 = viewID.findViewById(R.id.chart1); charts.add(chart1);
+        chart2 = viewID.findViewById(R.id.chart2); charts.add(chart2);
+        chart3 = viewID.findViewById(R.id.chart3); charts.add(chart3);
+        chart4 = viewID.findViewById(R.id.chart4); charts.add(chart4);
+        chart5 = viewID.findViewById(R.id.chart5); charts.add(chart5);
+        chart6 = viewID.findViewById(R.id.chart6); charts.add(chart6);
+        chart7 = viewID.findViewById(R.id.chart7); charts.add(chart7);
+
+        for(PieChart current_chart : charts)
+        {
+
+            current_chart.getDescription().setEnabled(false);
+            current_chart.setHoleRadius(15f);
+            current_chart.setTransparentCircleRadius(15f);
+            Legend l = current_chart.getLegend();
+            l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+            l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+            l.setOrientation(Legend.LegendOrientation.VERTICAL);
+            l.setDrawInside(false);
+        }
+    }
+
+
+    protected PieData generatePieDataContinent(Vector<CSVData> function_input, String continent, int counter)
+    {
+        ArrayList<PieEntry> entries1 = new ArrayList<>();
+        int local_counter = 0;
+        for(CSVData object : function_input)
+        {
+            if(object.getRegiune().equals(continent) && local_counter < counter)
+            {
+                entries1.add(new PieEntry(object.getCazuriTotale(), object.getNumeTara()));
+                local_counter++;
+            }
+        }
+
+        PieDataSet ds1 = new PieDataSet(entries1, "Lista Tari");
+        ds1.setColors(ColorTemplate.JOYFUL_COLORS);
+        ds1.setSliceSpace(1f);
+        ds1.setValueTextColor(Color.BLACK);
+        ds1.setValueTextSize(12f);
+
+        PieData d = new PieData(ds1);
+        d.setValueTypeface(tf);
+
+        return d;
+    }
+
+    protected PieData generatePieTops(Vector<CSVData> function_input, String Query)
+    {
+        ArrayList<PieEntry> entries1 = new ArrayList<>();
+
+        int[] maxs = {0, 0, 0};
+        String[] maxs_name = {"", "", ""};
+
+        if(Query.equals("TOP3TOTAL"))
+        {
+            for(CSVData object : function_input)
+            {
+                if(!object.getNumeTara().equals("Global"))
+                {
+                    for (int loop = 0; 3 > loop; loop++)
+                    {
+                        if (object.getCazuriTotale() > maxs[loop])
+                        {
+                            maxs[loop] = object.getCazuriTotale();
+                            maxs_name[loop] = object.getNumeTara();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        else if(Query.equals("TOP3DEATH"))
+        {
+            for(CSVData object : function_input)
+            {
+                if(!object.getNumeTara().equals("Global"))
+                {
+                    for (int loop = 0; 3 > loop; loop++)
+                    {
+                        if (object.getDecese() > maxs[loop])
+                        {
+                            maxs[loop] = object.getDecese();
+                            maxs_name[loop] = object.getNumeTara();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        else if(Query.equals("TOP324H"))
+        {
+            for(CSVData object : function_input)
+            {
+                if(!object.getNumeTara().equals("Global"))
+                {
+                    for (int loop = 0; 3 > loop; loop++)
+                    {
+                        if (object.getCazuriUltimele24ore() > maxs[loop])
+                        {
+                            maxs[loop] = object.getCazuriUltimele24ore();
+                            maxs_name[loop] = object.getNumeTara();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        for(int loop = 0; 3 > loop; loop++)
+        {
+            entries1.add(new PieEntry(maxs[loop], maxs_name[loop]));
+        }
+
+        PieDataSet ds1 = new PieDataSet(entries1, "Lista Tari");
+        ds1.setColors(ColorTemplate.MATERIAL_COLORS);
+        ds1.setSliceSpace(1f);
+        ds1.setValueTextColor(Color.BLACK);
+        ds1.setValueTextSize(12f);
+
+        PieData d = new PieData(ds1);
+        d.setValueTypeface(tf);
+
+        return d;
+    }
+
+    public Vector<CSVData> UploadData() throws IOException
+    {
+        Vector<CSVData> ListaTari = new Vector<CSVData>();
         InputStream input = getResources().openRawResource(R.raw.input);
         BufferedReader reader = new BufferedReader( new InputStreamReader(input, Charset.forName("UTF-8")));
         String line = "";
@@ -94,7 +296,7 @@ public class FirstFragment extends Fragment {
                 }
                 else {
                     CSVData tara = new CSVData(line.split(","));
-                    ListaTari.add(tara);
+                    ListaTari.addElement(tara);
                 }
                 lineCounter++;
             }
@@ -106,22 +308,5 @@ public class FirstFragment extends Fragment {
         return ListaTari;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        View rootView = inflater.inflate(R.layout.fragment_first, container, false);
-        TextView CSVText = rootView.findViewById(R.id.CSVText);
 
-        try
-        {
-            List<CSVData> objectList = UploadData();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-
-        return rootView;
-    }
 }
